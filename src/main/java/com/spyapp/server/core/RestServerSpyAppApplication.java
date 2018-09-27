df983dc443b6ecf7932146121a2cd5eaf121e05f
Albert Kuliński <albert.kulinski@hotmail.com>
2018-09-27 22:00:30 +0200

    initial commit

---
+++ src/main/java/com/spyapp/server/core/RestServerSpyAppApplication.java
@@ -0,0 +1,12 @@
+package com.spyapp.server.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServerSpyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestServerSpyAppApplication.class, args);
+    }
+}
